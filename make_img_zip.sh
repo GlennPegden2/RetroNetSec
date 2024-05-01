a34976557203737039c31da3665105ccb7e669f0
Glenn Pegden <glenn@pegden.com>
2024-05-01 18:27:45 +0100

    Fixed make_imzip.zip.sh comment addition

--- make_img_zip.sh
+++ make_img_zip.sh
@@ -1,3 +1,2 @@
-find .\hosts -type f \( -name "*.img" -o -name "*.txt" \) -exec zip RetroNetSecImages.zip {} +
-zip RetroNetSecImages.zip -z  "Preconfigured images of historic OSs for use with the RetroNetSec security preservation project"
+echo "Preconfigured images of historic OSs for use with the RetroNetSec security preservation project" | find ./hosts -type f \( -name "*.img" -o -name "*.txt" \) -exec zip -z RetroNetSecImages.zip {} +
 
